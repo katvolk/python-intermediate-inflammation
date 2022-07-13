d0a808ff1c1ff4f037df44b2750f0ae49fc4457b
katvolk <kat.volk@gmail.com>
2022-07-13 06:58:15 -0700

    added patient/doctor definitions and tests

--- tests/test_patient.py
+++ tests/test_patient.py
@@ -6,5 +6,55 @@ def test_create_patient():
 
     name = 'Alice'
     p = Patient(name=name)
-
     assert p.name == name
+
+
+def test_create_doctor():
+    from inflammation.models import Doctor
+
+    name = 'Dr. Who'
+    d = Doctor(name=name)
+    assert d.name == name
+
+
+def test_add_patient():
+    """make sure adding patients works"""
+    from inflammation.models import Doctor
+    from inflammation.models import Patient
+
+    d = Doctor(name='Dr. Who')
+    p = Patient(name='Amy')
+    d.add_patient(p)
+    assert d.patients is not None
+    assert len(d.patients) == 1
+
+def test_doctor_is_person():
+    """Check if a doctor is a person."""
+    from inflammation.models import Doctor, Person
+    doc = Doctor("Sheila Wheels")
+    assert isinstance(doc, Person)
+
+
+def test_patient_is_person():
+    """Check if a patient is a person. """
+    from inflammation.models import Patient, Person
+    alice = Patient("Alice")
+    assert isinstance(alice, Person)
+
+
+def test_add_mulitple_patients():
+    """add two patients, re-add first patient to make sure there
+    aren't any duplicates
+    """
+    from inflammation.models import Doctor
+    from inflammation.models import Patient
+
+    d = Doctor(name='Dr. Who')
+    p1 = Patient(name='Amy')
+    d.add_patient(p1)
+    p2 = Patient(name='Rory')
+    d.add_patient(p2)
+    p3 = Patient(name='Amy')
+    d.add_patient(p3)
+    assert d.patients is not None
+    assert len(d.patients) == 2
